8642abf3506885bcac8fb3b490165a2984a784ed
WSuarezCristian <125509550+WSuarezCristian@users.noreply.github.com>
2024-02-15 15:19:09 -0300

    PS5

---
+++ PS5/index.js
@@ -0,0 +1,69 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar-nav', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px'
+}) 
+
+
+// SECTION 2
+
+sr.reveal('.ps5', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '100px'
+}) 
+
+sr.reveal('.ambasps5', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px'
+}) 
+
+sr.reveal('.imgps5', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px'
+}) 
+
+
+sr.reveal('.imgps5_slime', {
+    duration: 3000,
+    origin: 'right',
+    distance: '100px'
+}) 
+
+
+// SECTION 3
+
+sr.reveal('.dual-img', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '100px'
+}) 
+
+
+sr.reveal('.presentacion-dualps5', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px'
+}) 
+
+
+
+// SECTION 4
+
+sr.reveal('.mas', {
+    duration: 3000,
+    origin: 'right',
+    distance: '100px'
+}) 
+
+sr.reveal('.box-accesorios', {
+    duration: 3000,
+    origin: 'left',
+    distance: '100px'
+}) 
+
+
